4f6dbffb88ec62811a7c2158f42f317a115fa5c6
MuhammadMarmash <hamada500014@gmail.com>
2023-07-14 09:38:16 +0300

    finished the basics

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,12 +6,17 @@ const title = document.querySelector("#title");
 const author = document.querySelector("#author");
 const pages = document.querySelector("#pages");
 const read = document.querySelector("#read");
+const content = document.querySelector("#content");
 
-// modal area
+// ui area
 function modal() {
     if (addBookModal.classList.contains("active")) {
         addBookModal.classList.remove("active");
         overlay.classList.remove("active");
+        title.value = "";
+        author.value = "";
+        pages.value = "";
+        read.checked = false;
     } else {
         addBookModal.classList.add("active");
         overlay.classList.add("active");
@@ -19,8 +24,70 @@ function modal() {
 }
 addBtn.addEventListener("click", modal);
 overlay.addEventListener("click", modal);
+function readBtnColor(isRead, readBtn) {
+    if (isRead) {
+        readBtn.textContent = "Read";
+        readBtn.classList.remove("btn-light-red");
+        readBtn.classList.add("btn-light-green");
+    } else {
+        readBtn.textContent = "Not read";
+        readBtn.classList.remove("btn-light-green");
+        readBtn.classList.add("btn-light-red");
+    }
+}
+function readBookBtn(book) {
+    book.read = book.read ? false : true;
+}
+function createBookCard(book) {
+    const card = document.createElement("div");
+    card.classList.add("card");
 
-// library area
+    const titleLapel = document.createElement("p");
+    titleLapel.textContent = `"${book.title}"`;
+    card.appendChild(titleLapel);
+
+    const authorLapel = document.createElement("p");
+    authorLapel.textContent = book.author;
+    card.appendChild(authorLapel);
+
+    const pagesLapel = document.createElement("p");
+    pagesLapel.textContent = `${book.pages} Pages`;
+    card.appendChild(pagesLapel);
+
+    const readBtn = document.createElement("button");
+    readBtn.classList.add("btn");
+    readBtnColor(book.read, readBtn);
+    readBtn.addEventListener("click", function (e) {
+        readBookBtn(book);
+        readBtnColor(book.read, readBtn);
+    });
+
+    card.appendChild(readBtn);
+
+    const removeBtn = document.createElement("button");
+    removeBtn.textContent = "Remove";
+    removeBtn.classList.add("btn");
+    card.appendChild(removeBtn);
+    removeBtn.addEventListener("click", function (e) {
+        removeBookUi(book.title, card);
+    });
+    return card;
+}
+function initBooksUi() {
+    library.books.forEach((book) => {
+        content.appendChild(createBookCard(book));
+    });
+}
+
+function addBookUi(book) {
+    content.appendChild(createBookCard(book));
+}
+
+function removeBookUi(title, card) {
+    content.removeChild(card);
+    library.removeBook(title);
+}
+// logic area
 class Book {
     constructor(
         title = "Unknown",
@@ -61,9 +128,16 @@ const library = new Library();
 
 function addBookToLibrary(e) {
     e.preventDefault();
-    library.addBook(
-        new Book(title.value, author.value, pages.value, read.checked)
+    const newBook = new Book(
+        title.value,
+        author.value,
+        pages.value,
+        read.checked
     );
+
+    library.addBook(newBook);
+    addBookUi(newBook);
     modal();
 }
 addBookForm.onsubmit = addBookToLibrary;
+initBooksUi();
